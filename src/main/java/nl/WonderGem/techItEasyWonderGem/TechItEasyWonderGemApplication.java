7b91e591322948f8da61c034fab8f021f0388d15
AramWondergem <aramwondergem@gmail.com>
2022-11-10 17:22:04 +0100

    Started the project

---
+++ src/main/java/nl/WonderGem/techItEasyWonderGem/TechItEasyWonderGemApplication.java
@@ -0,0 +1,13 @@
+package nl.WonderGem.techItEasyWonderGem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechItEasyWonderGemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechItEasyWonderGemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/WonderGem/techItEasyWonderGem/TechItEasyWonderGemApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.WonderGem.techItEasyWonderGem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyWonderGemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
